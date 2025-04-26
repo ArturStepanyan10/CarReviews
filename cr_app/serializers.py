7b324f8fad409012ca9405bd7ac1b1c959c3556b
ArturStepanyan10 <stepanyanartur110@gmail.com>
2025-04-26 17:02:07 +0300

    feat(Manufacturer): A serializer is registered for the Manufacturer entity

--- cr_app/serializers.py
+++ cr_app/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from cr_app.models import Country
+from cr_app.models import Country, Manufacturer
+
+
+class ManufacturerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Manufacturer
+        fields = ['id', 'name', 'country_id']
 
 
 class CountrySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Country
-        fields = ['id', 'name']
+        fields = ['id', 'name',]
 
