825b219c231e1b4b1daea00fb7b6cbb4afac24cc
ArturStepanyan10 <stepanyanartur110@gmail.com>
2025-04-27 00:21:31 +0300

    feat: Enabling Token Authentication

--- CarReviews/settings.py
+++ CarReviews/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'cr_app.apps.CrAppConfig',
-    'rest_framework'
+    'rest_framework',
+    #'django_extensions',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -110,6 +112,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 
 # Internationalization
