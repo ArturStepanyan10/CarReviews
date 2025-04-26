94629065487403a05ba350d960e4b3e960b22a3b
ArturStepanyan10 <stepanyanartur110@gmail.com>
2025-04-27 00:19:29 +0300

    feat: Adding an endpoint for the export_cars method

--- cr_app/urls.py
+++ cr_app/urls.py
@@ -2,15 +2,16 @@ from django.urls import path, include
 from rest_framework import routers
 
 from .views import CountryListCreateAPIView, CountryRetrieveUpdateDestroyAPIView, ManufacturerViewSet, CarViewSet, \
-    CommentViewSet
+    CommentViewSet, export_cars
 
 router = routers.SimpleRouter()
 router.register(r'manufacturers', ManufacturerViewSet, basename='manufacturers')
 router.register(r"cars", CarViewSet, basename='cars')
-router.register(r"comment", CommentViewSet, basename='comment')
+router.register(r"comments", CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('countries/', CountryListCreateAPIView.as_view(), name='country-list-create'),
     path('countries/<int:pk>/', CountryRetrieveUpdateDestroyAPIView.as_view(), name='country-detail'),
+    path('export-cars/', export_cars, name='export-cars'),
 ]
