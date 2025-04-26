5bf9c87996ab75cc6f125a2df3f2b5baa954db3e
ArturStepanyan10 <stepanyanartur110@gmail.com>
2025-04-26 16:16:45 +0300

    feat(Country): Implemented CRUD for the Country entity

--- cr_app/views.py
+++ cr_app/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from cr_app.models import Country
+from cr_app.serializers import CountrySerializer
+
+
+class CountryListCreateAPIView(APIView):
+    def get(self, request):
+        countries = Country.objects.all()
+        serializer = CountrySerializer(countries, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CountrySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CountryRetrieveUpdateDestroyAPIView(APIView):
+    def get(self, request, pk):
+        country = get_object_or_404(Country, pk=pk)
+        serializer = CountrySerializer(country)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        country = get_object_or_404(Country, pk=pk)
+        serializer = CountrySerializer(country, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        country = get_object_or_404(Country, pk=pk)
+        country.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
