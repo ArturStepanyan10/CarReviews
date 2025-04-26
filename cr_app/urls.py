20c191af890a533cdb95ecfa8bb325ace6596077
ArturStepanyan10 <stepanyanartur110@gmail.com>
2025-04-26 17:01:15 +0300

    feat(Manufacturer): Endpoints are registered for the Manufacturer entity

--- cr_app/urls.py
+++ cr_app/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from .views import CountryListCreateAPIView, CountryRetrieveUpdateDestroyAPIView
+from .views import CountryListCreateAPIView, CountryRetrieveUpdateDestroyAPIView, ManufacturerViewSet
+
+router = routers.SimpleRouter()
+router.register(r'manufacturers', ManufacturerViewSet, basename='manufacturers')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('countries/', CountryListCreateAPIView.as_view(), name='country-list-create'),
     path('countries/<int:pk>/', CountryRetrieveUpdateDestroyAPIView.as_view(), name='country-detail'),
 ]
